3b5a549d7449e6e1a17419de97126832a25f59f0
D-Dietrich <dadietrich@mail.fhsu.edu>
2022-03-20 11:21:23 -0500

    Link Database

--- model/database.php
+++ model/database.php
@@ -1,10 +1,10 @@
 <?php
-$dsn = 'mysql:host=localhost; dbname=zippyusedautos';
-$username = 'root';
-
+$dsn = 'mysql:host=acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; dbname=u9w19g3yjxz55x1z';
+$username = 'bzrzfxgclgs3szbq';
+$p = 'poiq15av52j46npe';
 
 try {
-    $db =new PDO($dsn, $username);
+    $db =new PDO($dsn, $username, $p);
 
 
 } catch (PDOException $e) {
